31faa7413b4082749b164a457ed25f80e79281e7
Krzysztof Cel <krzycel789@student.polsl.pl>
2021-01-12 12:31:41 +0100

    rand + cout tablicy (z1)

--- Kolt1.cpp
+++ Kolt1.cpp
@@ -1,11 +1,35 @@
 ﻿#include <iostream>
+#include <time.h>
+#include <iomanip>
+using namespace std;
 
-#define Zadanie1 
+#define Zadanie1
 
+#define RND (rand()+1.0)/ (RAND_MAX+2.0)
 #ifdef Zadanie1
+int main()
+{
+	double tab[5][7] = {};
+	int i, j;
 
-// TU ROZWIĄZUJ ZADANIE 1:
+	for (i = 0; i < 5; i++)
+	{
+		for (j = 0; j < 7; j++)
+		{
+			tab[i][j] = -5 + RND * (12 + 5);
+		}
+	}
+	
+	for (i = 0; i < 5; i++)
+	{
+		for (j = 0; j < 7; j++)
+		{
+			cout << setprecision(2) << tab[i][j] << "\t";
+		}
+		cout << endl;
+	}
 
+}
 #endif
 
 #ifdef Zadanie2
