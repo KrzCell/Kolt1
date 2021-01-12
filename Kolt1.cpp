3dba9bf2825ab06500d55280ec4f8b4d22a6fffa
Krzysztof Cel <krzycel789@student.polsl.pl>
2021-01-12 12:03:31 +0100

    poczatek

---
+++ Kolt1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+#define Zadanie1 
+
+#ifdef Zadanie1
+
+// TU ROZWIĄZUJ ZADANIE 1:
+
+#endif
+
+#ifdef Zadanie2
+
+	// TU ROZWIĄZUJ ZADANIE 2:
+
+#endif
+
+#ifdef Zadanie3
+
+	// TU ROZWIĄZUJ ZADANIE 3:
+
+#endif
+
+#ifdef Zadanie4
+
+	// TU ROZWIĄZUJ ZADANIE 4:
+
+#endif
